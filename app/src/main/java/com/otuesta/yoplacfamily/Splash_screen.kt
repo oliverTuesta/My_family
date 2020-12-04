d8dfa1881081252f5975de845c18995848a08996
OliverTuesta <oliver.jtuesta@gmail.com>
2020-12-04 12:34:30 -0500

    creación del proyecto y de algunos fragments

---
+++ app/src/main/java/com/otuesta/yoplacfamily/Splash_screen.kt
@@ -0,0 +1,11 @@
+package com.otuesta.yoplacfamily
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Splash_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
