fdcf9660ecb0cffa0cb1a0930c68351e7afee6a7
OliverTuesta <oliver.jtuesta@gmail.com>
2020-12-09 10:39:37 -0500

    diseño light terminado

--- app/src/main/java/com/otuesta/myfamily/MainActivity.kt
+++ app/src/main/java/com/otuesta/myfamily/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.otuesta.myfamily
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setActionBar(findViewById(R.id.tbMain))
+
+        val bnvMenu = findViewById<BottomNavigationView>(R.id.bnvMenu) as BottomNavigationView
+
+        configNav(bnvMenu)
+
     }
+
+    private fun configNav(bnvMenu: BottomNavigationView) {
+        NavigationUI.setupWithNavController(bnvMenu, Navigation.findNavController(this, R.id.fragContent))
+    }
+
+
 }
